183046f36b51311bbec9b8fd1c41c3014a9bbbc1
22ssison <22ssison@wakatipu.school.nz>
2025-04-09 15:54:47 +1200

    polished up doc strings for each of my current functions.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ user_cart = []
 
 # integer
 def check_quantity(txt):
+    """This function is specific to how many quesadillas the user wants. It sets bounderies to deal with unexpected inputs."""
     while True:
         try:
             x = int(input(txt)) # checks if this is an integer
@@ -31,6 +32,7 @@ def check_quantity(txt):
 
 # string
 def check_response(txt):
+    """This function is only applied to yes or no answers and deals with potential input errors."""
     while True:
         x = input(txt).strip().lower() # strips any potential accidental spaces and makes it all lowercase.
         if x in ["y", "yes", "n", "no"]: 
@@ -44,7 +46,7 @@ def check_response(txt):
 
 # integer
 def check_deliver_int(txt):
-    """This function is to check each integer user input to control the input types and prevent the program from crashing"""
+    """This function is specific to checking the user's input for the pickup or delivery prompt."""
     while True:
         try:
             x = int(input(txt)) # checks if this is an integer
@@ -59,7 +61,7 @@ def check_deliver_int(txt):
 
 # integer
 def check_order_int(txt):
-    """This function is to check each integer user input to control the input types and prevent the program from crashing"""
+    """This function is specific to the user's selection of quesadillas according to the menu."""
     while True:
         try:
             x = int(input(txt)) # won't count floats either
@@ -72,9 +74,9 @@ def check_order_int(txt):
             print("Invalid Input. Please enter a number.")
             print()
 
-
+# integer
 def check_menu_int(txt):
-    """This function is to check each integer user input to control the input types and prevent the program from crashing"""
+    """This function is specific to the user's operation selection in the main menu system."""
     while True:
         try:
             x = int(input(txt))
@@ -85,9 +87,9 @@ def check_menu_int(txt):
         except ValueError:
             print("Invalid Input. Please enter a number.")
 
-
+# string
 def check_string(txt):
-    """This function checks each user string input and prevents the user from not entering anything and the program saving nothing."""
+    """This function checks strings and prevents the program from saving nothing."""
     while True:
         x = input(txt).strip()
         if x: # if it contains a value, it will be the same as "if True:"
@@ -95,9 +97,9 @@ def check_string(txt):
         print("Input can't be blank. Please enter something.")
         print()
 
-
-def phone_characters(txt):
-    """This function checks the number of characters entered in when the user has entered their phone number, and provides a barrier of how many characters they should enter in."""
+#integer
+def check_phone_characters(txt):
+    """This function checks the number of integer characters entered for the phone number, and provides a range of how many characters they should enter in."""
     while True:
         try:
             x = int(input(txt)) # checks if this is an integer
@@ -108,6 +110,7 @@ def phone_characters(txt):
         except ValueError:
             print("Invalid phone number. Please enter a 9 or 10 digit number.")
 
+
 def delivery_user_info():
     """This function saves the user's info which is specific for the delivery option."""
     print()
@@ -115,7 +118,7 @@ def delivery_user_info():
     print()
     first_name = check_string("First Name: ").lower().strip()
     address = check_string("Address: ") # fix? .lower().strip()
-    phone_number = phone_characters("Phone Number: ") 
+    phone_number = check_phone_characters("Phone Number: ") 
     user_info.append([first_name.capitalize(), address, phone_number])
     print()
     print("Details Saved.")
@@ -133,7 +136,7 @@ def pick_up_user_info():
 
 
 def pickup_or_delivery():
-    """This funciton asks the user to choose whether they want to pick up the food or get it delivered. It also determines what information is asked of them."""
+    """This funciton asks the user to choose whether they want to pick up or get their items delivered."""
     print()
     option = check_deliver_int("Please select from the following options: \n1) Pick up \n2) Delivery\n> ")
     if option == 1:
@@ -194,17 +197,21 @@ def quesadillas_menu():
     if order in ["y", "yes"]:
         add_to_cart()
 
-# test + fix after creating working menu system
+
 def calculate():
+    """This function calculates the total cost of the user's items."""
     for quesadilla in user_cart:
         total = sum(quesadilla[2])
-        print(f"Total Cost: ${total:.2f}")
+        print(f"Total Cost: ${total:.2f}") # make sure the output is 2dp
 
+# fix
 def cart_menu():
     """This function holds the cart menu for the user, which allows them to add additional items into their cart, remove items into their cart or simply view the items in their cart. """
     pass # add while loop, so that it asks the user again and again if they want to add more or they are ready to check out.
 
+
 def display_cart():
+    """This funciton displays all the items the user currently has within their cart."""
     print()
     print("🛒 Your Cart:")
     count = 0 # to keep count of how many elements three are in the list.
@@ -222,6 +229,7 @@ def display_cart():
 
 #fix
 def checkout():
+    """This function allows the user to secure their order once they are happy with all the items they have in their cart."""
     if len(user_cart) == 0:
         print()
         print("You have 0 items in your cart.")
@@ -236,7 +244,7 @@ def checkout():
 
 
 def main_menu():
-    """This function holds the main menu. It displays a range of different options the user can do in this ordering programme and executes them accordingly - by calling hte other function"""
+    """This function holds the main menu. It displays a range of different options the user can do in this ordering programme and executes them accordingly - by calling the other functions."""
     print("Welcome to Queenstown Quesadilla's online orders")
     print()
     input("Press enter to continue")
@@ -264,7 +272,8 @@ def main_menu():
         if response not in ["y", "yes"]:
             break
 
-if __name__ == "__main__": # python convention, anything under this  is ran only in this page.
+
+if __name__ == "__main__": # python convention, anything under this is ran only inside this page.
    main_menu()
 
 """
@@ -274,6 +283,4 @@ tweaks:
 - in the check out box, capitalise the first letter of the user's name + address + add a maximum phone number
 
 - check each and evefy integer input and do not allow any inputs to be a negative integer/float/etc
-
-
 """
